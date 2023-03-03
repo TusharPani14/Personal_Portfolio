5664a4bd8dd64323384372f87a02f257ce2dc178
Tushar Panigrahi <tusharpanigrahi66@gmail.com>
2023-03-03 22:19:00 +0530

    small change4

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const path = require("path");
 const app = express();
 dotenv.config()
 app.use(cors());
+
+//Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://personal-portfolio-i6wi.onrender.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 app.use(express.json());
 app.use("/", router);
 const PORT=process.env.PORT||5000
